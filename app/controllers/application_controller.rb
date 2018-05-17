a04965d74bc7ed24cbc5ac34c3e7bfbd8bbecff9
ivan bettin <ibanb040@gmail.com>
2018-05-17 13:01:10 +0000

    funcionalidad de registrarse y loguin funcionando, se configuraron los parametros en el controlador para devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+before_action :configure_devise_params, if: :devise_controller?
+
+	def configure_devise_params
+		devise_parameter_sanitizer.permit(:sign_up) do |user|
+			user.permit(:email, :name_1, :name_2, :last_name_1, :last_name_2, :number, :username, :password, :password_confirmation, :departament_id)
+		end
+	end
+
 end
